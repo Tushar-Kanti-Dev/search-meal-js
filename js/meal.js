73b6a1a6b44a284bfab19d9a2b3e3fd49aa029bd
Muhammad Shamsojjaman <tushaardev.td@gmail.com>
2022-11-10 22:04:47 +0600

    'click to add' function added

--- js/meal.js
+++ js/meal.js
@@ -15,7 +15,7 @@ const displaySerachResult = meals => {
         const div = document.createElement('div');
         div.classList.add('col');
         div.innerHTML = `
-        <div class="card container">
+        <div onclick="loadMealDetail(${meal.idMeal})" class="card container">
                     <img src="${meal.strMealThumb}" class="card-img-top img-fluid" alt="...">
                     <div class="card-body">
                         <h5 class="card-title">${meal.strMeal}</h5>
@@ -24,8 +24,35 @@ const displaySerachResult = meals => {
                 </div>
         `
         searchResult.appendChild(div)
-        console.log(meal);
+            // console.log(meal);
 
     });
     // console.log(meals)
+}
+const loadMealDetail = mealId => {
+    const url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealId}`
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displayMealDetails(data.meals[0]))
+        // console.log(url);
+}
+const displayMealDetails = meal => {
+    const randomDisplay = document.getElementById('card');
+    const div = document.createElement('div');
+    div.classList.add('row');
+    div.innerHTML = `
+    <div class="col-md-5">
+                    <img src="${meal.strMealThumb}" class="img-fluid rounded-start" alt="...">
+                </div>
+                <div class="col-md-7">
+                    <div class="card-body">
+                        <h5 class="card-title">${meal.strMeal}</h5>
+                        <p class="card-text">${meal.strInstructions.slice(0,150)}</p>
+                        <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                    </div>
+                </div>
+    `;
+    randomDisplay.appendChild(div);
+
+    console.log(meal);
 }
